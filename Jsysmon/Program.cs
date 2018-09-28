52d4a22a115718e480a673ed379ac6fde8f463d7
johnsie <steekyjim@yahoo.com>
2018-09-28 10:23:22 +0100

    Test @ W

--- Jsysmon/Program.cs
+++ Jsysmon/Program.cs
@@ -13,7 +13,7 @@ namespace Jsysmon
         /// </summary>
         [STAThread]
         static void Main()
-        { 
+        {  
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
